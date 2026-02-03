3c8fd78d1351856447aeeb7a8475909397a9f9e6
r4m4 <ramahany317@gmail.com>
2026-02-03 20:00:47 +0200

    adding pydantic settings config for the env

---
+++ helpers/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+class Settings(BaseSettings): 
+    APP_NAME : str
+    APP_VERSION : str
+
+    class Config: 
+        env_file = '.env'
+
+def get_settings(): 
+    return Settings()
--- routes/base.py
+++ routes/base.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter
-import os
+from fastapi import APIRouter, Depends
+from helpers.config import get_settings, Settings
 api_router = APIRouter(
     prefix="/api/v1", 
     tags=["Base"]
 )
+
 @api_router.get('/')
-async def welcome(): 
+async def welcome(app_settings : Settings = Depends(get_settings)): 
     return {
-        "APP_NAME" : os.environ['APP_NAME'],
-        "APP_VERSION" : os.environ['APP_VERSION']
+        "APP_NAME" : app_settings.APP_NAME,
+        "APP_VERSION" : app_settings.APP_VERSION
     }
